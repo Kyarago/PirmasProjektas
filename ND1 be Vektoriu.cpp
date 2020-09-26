f523b13a224cd8f8bea6770644954f696894d5a7
Aurimas Auga <71488215+Kyarago@users.noreply.github.com>
2020-09-26 14:25:10 +0300

    Add files via upload

--- ND1 be Vektoriu.cpp
+++ ND1 be Vektoriu.cpp
@@ -32,32 +32,41 @@ struct duomenys {
         for (int i = 0; i < n; i++) {
             cout << "Iveskite " << i + 1 << "-aji vada: ";
             cin >> grupe[i].Vardas;
-        }
-        for (int i = 0; i < n; i++) {
+//        }
+//        for (int i = 0; i < n; i++) {
             cout << "Iveskite " << i + 1 << "-ojo studento pavarde: ";
             cin >> grupe[i].Pavarde;
-        }
-        for (int i = 0; i < n; i++) {
+//        }
+//        for (int i = 0; i < n; i++) {
             cout << "Iveskite " << i + 1 << "-ojo studento egzamina: ";
             cin >> grupe[i].Egzaminas;
         }
         int m = 0;
         cout << "Iveskite kiek nd pazymiu buvo: ";
         cin >> m;
+        int c;
         for (int i = 0; i < n; i++) {
             cout << "Iveskite " << i + 1 << "-ojo studento nd pazymius: ";
             for (int k = 0; k < m; k++) {
                 cin >> grupe[i].Namu[k];
-//                grupe[i].Galutinis = grupe[i].Galutinis + grupe[i].Namu[k];
-            }
-            size_t size = sizeof(grupe[i].Namu) / sizeof(grupe[i].Namu[0]); //įvestų elementų skaičius
-            std::sort(grupe[i].Namu, grupe[i].Namu + size);
-            for (size_t o = 0; o < size; o++) {
-                cout << grupe[i].Namu[o] << ' ';
                 grupe[i].Galutinis = grupe[i].Galutinis + grupe[i].Namu;
             }
-                        
+            c = sizeof(grupe[i].Namu) / sizeof(grupe[i].Namu[0]); //ivestu elementu skaicius
+            std::sort(grupe[i].Namu, grupe[i].Namu + c); // kadangi nuėjote sudėtingu būdu tai keikia naudoti tokią sintaksę t.y. pirmo elemento adresas ir paskutinio elemento adresas gaunamas iš pirmo plius elementų skaičius - rezultatas tokios sudėties ir yra paskutinio elemento adresas.
+//            grupe[i].Galutinis = grupe[i].Galutinis + grupe[i].Namu; //Kodėl k?
         }
+//            int c;
+//            c = sizeof(grupe[i].Namu) / sizeof(grupe[i].Namu[0]); //įvestų elementų skaičius
+//            std::sort(grupe[i].Namu, grupe[i].Namu + c);
+//            if (c % 2 == 1)  
+//                mediana = grupe[i].Namu[c / 2];
+//            else 
+//                mediana = (float)(grupe[i].Namu[c / 2 - 1] + grupe[i].Namu[c / 2]) * 0.5;
+//            for (int o = 0; o < c; o++) {
+//                cout << grupe[i].Namu[o] << ' ';
+//            }                        
+//        }
+//            grupe[i].Galutinis = grupe[i].Galutinis + (int)grupe[i].Namu;
 //        int a[] = { 1,5,4,2,3,6,7,8,2 };
 //        size_t size = sizeof(a) / sizeof(a[0]); //įvestų elementų skaičius
 //        std::sort(a, a + size); // surūšiuoti masyvą a nuo nulinio elemento iki n
@@ -73,9 +82,10 @@ struct duomenys {
         for (int i = 0; i < n; i++) {
             grupe[i].Galutinis = (float)grupe[i].Galutinis / m;
             grupe[i].Galutinis = grupe[i].Galutinis * 0.4 + 0.6 * grupe[i].Egzaminas;
-            cout << std::setw(20) << grupe[i].Vardas 
-                << std::setw(20) << grupe[i].Pavarde 
-                << std::setw(20) << std::setprecision(2) << std::fixed << grupe[i].Galutinis << endl;
+            cout << std::setw(20) << grupe[i].Vardas
+                << std::setw(20) << grupe[i].Pavarde
+                << std::setw(20) << std::setprecision(2) << std::fixed << grupe[i].Galutinis;
+//                << std::setw(20) << std::setprecision(2) << std::fixed << mediana << endl;
         }
         delete[] grupe;
     }
