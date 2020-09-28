ba4d12e005e82dfdc47615dea191fed36e2dd0a8
Aurimas Auga <71488215+Kyarago@users.noreply.github.com>
2020-09-28 13:58:51 +0300

    Add files via upload

--- ND1 Su Vektoriais.cpp
+++ ND1 Su Vektoriais.cpp
@@ -17,7 +17,7 @@ using std::sort;
 struct duomenys {
     string Vardas = "";
     string Pavarde = "";
-    int Namu[10];
+    vector<int> Namu;
     int Egzaminas;
     string ivestis = "";
     float Galutinis = 0;
@@ -28,6 +28,7 @@ int main()
 {
     vector<duomenys> grupe;
     duomenys stud;
+    int nd = 1;
     int n = 0;
     cout << "Iveskite studentu skaiciu: ";
     cin >> n;
@@ -43,33 +44,46 @@ int main()
             cout << "Ivestas negalimas pazimys, veskite is naujo: ";
             cin >> stud.Egzaminas;
         }
+        cout << "Iveskite " << i + 1 << "-ojo studento nd pazymius, kai noresite sustoti, irasykite 0: \n";
+        while (nd != 0) {
+//            cout << "Iveskite " << i +1 << "-ojo studento nd pazymius, kai noresite sustoti, irasykite 0: \n";
+            cin >> nd;
+            if (nd > 0 && nd < 11) {
+                stud.Galutinis = stud.Galutinis + nd;
+                stud.Namu.push_back(nd);
+            }
+            else if (nd < 0 || nd > 10) {
+                cout << "Klaida, ivestas neteisingas pazimys \n";
+            }
+        }
         
         int c;
-        int m = 0;
-        cout << "Iveskite nd skaiciu: ";
-        cin >> m;
-        if (m > 0 & m < 11) cout << "Iveskite " << i + 1 << "-ojo studento nd pazymius: ";
-        for (int k = 0; k < m; k++) {
-            cin >> stud.Namu[k];
-            while (stud.Namu[k] < 1 || stud.Namu[k] > 10) {
-                cout << "Ivestas negalimas pazimys, veskite is naujo: ";
-                cin >> stud.Namu[k];
-            }
-            stud.Galutinis = stud.Galutinis + stud.Namu[k];
-            c = sizeof(stud.Namu) / sizeof(stud.Namu[0]);
-        }
-        cout << "C = " << c;
-        std::sort(stud.Namu, stud.Namu + m);
-        if (m % 2==1)
-            stud.mediana = stud.Namu[m / 2];
+//        int m = 0;
+//        cout << "Iveskite nd skaiciu: ";
+//        cin >> m;
+//        if (m > 0 & m < 11) cout << "Iveskite " << i + 1 << "-ojo studento nd pazymius: ";
+//        for (int k = 0; k < m; k++) {
+//            cin >> stud.Namu[k];
+//            while (stud.Namu[k] < 1 || stud.Namu[k] > 10) {
+//                cout << "Ivestas negalimas pazimys, veskite is naujo: ";
+//                cin >> stud.Namu[k];
+//            }
+//            stud.Galutinis = stud.Galutinis + stud.Namu[k];
+//            c = sizeof(stud.Namu) / sizeof(stud.Namu[0]);
+        c = stud.Namu.size();
+//        }
+        cout << "Namu darbu pazymiu skaicius: " << c << endl;
+        std::sort(stud.Namu.begin(), stud.Namu.end());
+        if (c % 2==1)
+            stud.mediana = stud.Namu[c / 2];
         else
-            stud.mediana = (stud.Namu[m / 2 - 1] + stud.Namu[m / 2]) / 2;
+            stud.mediana = (stud.Namu[c / 2 - 1] + stud.Namu[c / 2]) / 2;
 
-        stud.Galutinis = stud.Galutinis / m;
+        stud.Galutinis = stud.Galutinis / c;
         stud.Galutinis = stud.Galutinis * 0.4 + 0.6 * stud.Egzaminas;
         grupe.push_back(stud);
     }
-    cout << "Jei norite matyti viduriki - iveskite v, jei mediana - iveskite m   ";
+    cout << "Jei norite matyti viduriki - iveskite v, jei mediana - iveskite m:   ";
     cin >> stud.ivestis;
     while (stud.ivestis != "v" && stud.ivestis != "m") {
         cout << "Ivestas negalimas pasirinkimas, veskite is naujo: ";
