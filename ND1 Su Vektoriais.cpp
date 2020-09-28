8437ec5230c4ec9515dd6ef0843d9ad7522541ad
Aurimas Auga <71488215+Kyarago@users.noreply.github.com>
2020-09-28 10:21:57 +0300

    Add files via upload

--- ND1 Su Vektoriais.cpp
+++ ND1 Su Vektoriais.cpp
@@ -17,8 +17,9 @@ using std::sort;
 struct duomenys {
     string Vardas = "";
     string Pavarde = "";
-    int Namu[10];
+    vector<int> Namu;
     int Egzaminas;
+    string ivestis = "";
     float Galutinis = 0;
     float mediana;
 };
@@ -27,6 +28,7 @@ int main()
 {
     vector<duomenys> grupe;
     duomenys stud;
+    int nd = 0;
     int n = 0;
     cout << "Iveskite studentu skaiciu: ";
     cin >> n;
@@ -38,39 +40,77 @@ int main()
         cin >> stud.Pavarde;
         cout << "Iveskite " << i + 1 << "-ojo studento egzamina: ";
         cin >> stud.Egzaminas;
-        
-        int m = 0;
-        cout << "Iveskite nd skaiciu: ";
-        cin >> m;
-        if (m>0) cout << "Iveskite " << i + 1 << "-ojo studento nd pazymius: ";
-        for (int k = 0; k < m; k++) {
-            cin >> stud.Namu[k];
-            stud.Galutinis = stud.Galutinis + stud.Namu[k];
+        while (stud.Egzaminas < 1 || stud.Egzaminas > 10) {
+            cout << "Ivestas negalimas pazimys, veskite is naujo: ";
+            cin >> stud.Egzaminas;
         }
+        while (nd != 0) {
+            cout << "Iveskite studento nd pazymius, kai noresite sustoti, irasykite 0: ";
+            cin >> nd;
+            if (nd > 0 && nd < 11) {
+                stud.Galutinis = stud.Galutinis + nd;
+            }
+            else if (nd < 0 || nd > 10) {
+                cout << "Klaida, ivestas neteisingas pazimys ";
+            }
+        }
+        
+//        int c;
+//        int m = 0;
+//        cout << "Iveskite nd skaiciu: ";
+//        cin >> m;
+//        if (m > 0 & m < 11) cout << "Iveskite " << i + 1 << "-ojo studento nd pazymius: ";
+//        for (int k = 0; k < m; k++) {
+//            cin >> stud.Namu[k];
+//            while (stud.Namu[k] < 1 || stud.Namu[k] > 10) {
+//                cout << "Ivestas negalimas pazimys, veskite is naujo: ";
+//                cin >> stud.Namu[k];
+//            }
+//            stud.Galutinis = stud.Galutinis + stud.Namu[k];
+//            c = sizeof(stud.Namu) / sizeof(stud.Namu[0]);
+//        }
+//        cout << "C = " << c;
+//        std::sort(stud.Namu, stud.Namu + m);
+//        if (m % 2==1)
+//            stud.mediana = stud.Namu[m / 2];
+//        else
+//            stud.mediana = (stud.Namu[m / 2 - 1] + stud.Namu[m / 2]) / 2;
 
-        std::sort(stud.Namu, stud.Namu + m);
-        if (m % 2==1)
-            stud.mediana = stud.Namu[m / 2];
-        else
-            stud.mediana = (stud.Namu[m / 2 - 1] + stud.Namu[m / 2]) / 2;
-
-        stud.Galutinis = stud.Galutinis / m;
-        stud.Galutinis * 0.4 + 0.6 * stud.Egzaminas;
+//        stud.Galutinis = stud.Galutinis / m;
+//        stud.Galutinis = stud.Galutinis * 0.4 + 0.6 * stud.Egzaminas;
         grupe.push_back(stud);
     }
-    
-    cout << std::left
-        << std::setw(20) << "Vardas "
-        << std::setw(20) << "Pavarde"
-        << std::setw(20) << "Galutinis"
-        << "Mediana \n";
-    cout << "-------------------------------------------------\n";
-    for (auto& d : grupe) {
+    cout << "Jei norite matyti viduriki - iveskite v, jei mediana - iveskite m   ";
+    cin >> stud.ivestis;
+    while (stud.ivestis != "v" && stud.ivestis != "m") {
+        cout << "Ivestas negalimas pasirinkimas, veskite is naujo: ";
+        cin >> stud.ivestis;
+    }
+    if (stud.ivestis == "v") {
         cout << std::left
-            << std::setw(20) << d.Vardas
-            << std::setw(20) << d.Pavarde
-            << std::setw(20) << std::setprecision(2) << std::fixed << d.Galutinis
-            << std::setw(20) << std::setprecision(2) << std::fixed << d.mediana << "\n";
+            << std::setw(20) << "Vardas "
+            << std::setw(20) << "Pavarde"
+            << "Galutinis \n";
+        cout << "-------------------------------------------------\n";
+        for (auto& d : grupe) {
+            cout << std::left
+                << std::setw(20) << d.Vardas
+                << std::setw(20) << d.Pavarde
+                << std::setw(20) << std::setprecision(2) << std::fixed << d.Galutinis << "\n";
+        }
+    }
+    else if (stud.ivestis == "m") {
+        cout << std::left
+            << std::setw(20) << "Vardas "
+            << std::setw(20) << "Pavarde"
+            << "Mediana \n";
+        cout << "-------------------------------------------------\n";
+        for (auto& d : grupe) {
+            cout << std::left
+                << std::setw(20) << d.Vardas
+                << std::setw(20) << d.Pavarde
+                << std::setw(20) << std::setprecision(2) << std::fixed << d.mediana << "\n";
+        }
     }
     cout << endl;
     grupe.clear();
