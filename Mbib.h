d5c385568dc3a55c3d14d1ddeb94b85457910b5b
Aurimas Auga <71488215+Kyarago@users.noreply.github.com>
2020-10-13 11:59:00 +0300

    Add files via upload

---
+++ Mbib.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <random>
+#include <numeric>
+#include <fstream>
+#include <sstream>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::sort;
+
+struct duomenys {
+    string Vardas = "";
+    string Pavarde = "";
+    vector<int> Namu;
+    int Egzaminas;
+    float Galutinis = 0;
+    float mediana;
+};
+
+bool Tvarkymas(const duomenys& pirmas, const duomenys& antras);
+void ivedimas(vector<duomenys> &grupe);
+void isvedimas(vector<duomenys>& grupe);
